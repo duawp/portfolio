b666b4688b28ab9c141158dfd981791357683c23
Malek Chaabane <168550865+yqr5@users.noreply.github.com>
2024-09-22 12:49:31 +0100

    these are the new changes

--- script.js
+++ script.js
@@ -20,20 +20,3 @@ hoverSound.volume = 0.2; // Adjust the volume here
 
 // Combine both sets of buttons into one array
 var allButtons = [...navButtons, ...socialButtons];
-
-// Add hover event to each button (nav links and social media icons)
-allButtons.forEach(function(button) {
-    button.hasPlayed = false; // Set a flag for each button to track if sound has played
-    
-    button.onmouseover = function() {
-        if (!button.hasPlayed) {   // Only play sound if it hasn't played yet for this button
-            hoverSound.currentTime = 0; // Reset sound to the beginning
-            hoverSound.play();
-            button.hasPlayed = true; // Mark sound as played for this button
-        }
-    };
-
-    button.onmouseout = function() {
-        button.hasPlayed = false; // Reset the flag when the user moves the mouse away
-    };
-});
